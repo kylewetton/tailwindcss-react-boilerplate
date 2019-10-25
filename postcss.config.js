39c25b8ae278bfa6f9b80ffb571d53d0103b5e63
kylewetton <kylewetton@me.com>
2019-10-25 21:12:42 +1300

    v1

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+const tailwindcss = require("tailwindcss");
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.js", "./src/**/*.html"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [
+    tailwindcss("./tailwindcss/tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
+};
---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/tailwind.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-200 flex justify-center items-center">
+      <div className="p-4 bg-gray-900 rounded-lg shadow">
+        <h1 className="tracking-widest uppercase text-sm font-bold text-white">
+          React with TailwindCSS
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
